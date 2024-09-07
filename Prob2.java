4d3799a666d47fde187d5aa3e1030602f1e3445b
Mannu Kumar Verma <142576550+LightWithDarkness@users.noreply.github.com>
2024-09-07 21:03:57 +0530

    Create Prob2.java

---
+++ Prob2.java
@@ -0,0 +1,20 @@
+// problem = https://leetcode.com/problems/product-of-array-except-self/submissions/1382203204/?envType=study-plan-v2&envId=leetcode-75
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
